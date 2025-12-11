ae477191fd5d93055b190a6081a2abeb34be765a
Denny981 <dennyzhang19@gmail.com>
2025-12-10 21:18:47 -0500

    day 2 & execution instructions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Advent-of-Code-
 # The directory is structured so it is in years and in the src folder is where the days are for each respective year
+# Run by inputting npm start day _ (Number) in the aoc2025 folder
+# Ex. npm start day 1
--- aoc2025/src/day02/index.ts
+++ aoc2025/src/day02/index.ts
@@ -1,17 +1,51 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string): string[] =>   rawInput.split(",").map(s => s.trim()).filter(Boolean);
+
+function isInvalidId(value: number){
+  const valueString = value.toString();
+  // Part 2
+  return /^(\d+)\1+$/.test(valueString);
+  // Part 1
+  // const left = valueString.slice(0, valueString.length / 2);
+  // const right = valueString.slice(valueString.length / 2);
+  // return left === right;
+}
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  // console.log(input);
+  const numsInput = input.map(p => p.split("-").map(Number));
+  // console.log(numsInput);
+  const inValidIds: number[] = [];
+  const [[start, end]] = numsInput;
+  for (const [start, end] of numsInput){
+    for (let x = start!; x <= end!; x++){
+      if(isInvalidId(x)){
+        inValidIds.push(x);
+      }
+    }
+  }
+  // console.log(inValidIds);
+  return inValidIds.reduce((s, v) => s + v);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  // console.log(input);
+  const numsInput = input.map(p => p.split("-").map(Number));
+  // console.log(numsInput);
+  const inValidIds: number[] = [];
+  const [[start, end]] = numsInput;
+  for (const [start, end] of numsInput){
+    for (let x = start!; x <= end!; x++){
+      if(isInvalidId(x)){
+        inValidIds.push(x);
+      }
+    }
+  }
+  // console.log(inValidIds);
+  return inValidIds.reduce((s, v) => s + v);
 };
 
 run({
